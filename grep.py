ce5a30c1475676e29b580417b03d14727e967a93
celinekeisja <celinekeisja@gmail.com>
2019-10-16 16:37:20 +0800

    Update grep.py

--- grep.py
+++ grep.py
@@ -4,19 +4,19 @@ import os
 import glob
 
 
-def grep(given_file, path, expr):
+def grep(file_pattern, path, expr):
     compiled = re.compile(r'{}.*'.format(expr))
     for r, d, f in os.walk(path):
-        for text in f:
+        for fname in f:
             d_path = os.path.abspath(r)
-            name = os.path.basename(text)
+            name = os.path.basename(fname)
             abspath = os.path.join(d_path, name)
             for apath in glob.glob(abspath):
                 with open(apath, 'r') as f:
                     text = f.read()
                     match = compiled.finditer(text)
                     for m in match:
-                        print(f'{file} \t {m.group(0)}')
+                        print(f'{name} \t {m.group(0)}')
 
 
 if __name__ == "__main__":
@@ -29,6 +29,5 @@ if __name__ == "__main__":
     file = args.file_pattern
     file_path = args.path
     expression = args.regular_expression
-
     grep(file, file_path, expression)
 
